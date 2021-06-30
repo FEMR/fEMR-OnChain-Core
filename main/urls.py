23b48980552a7e078125b31d151b9a060660c674
Sean Batzel <romulus108@protonmail.com>
2021-06-30 10:08:05 -0400

    Fixed a URL error.

--- main/urls.py
+++ main/urls.py
@@ -10,7 +10,7 @@ from main.admin_views import add_user_to_campaign, add_users_to_campaign, admin_
     search_users_view, update_user_view, update_user_password_view
 from .api_views import UserViewSet, GroupViewSet, PatientViewSet, PatientEncounterViewSet, InstanceViewSet, CampaignViewSet
 from .auth_views import all_locked, not_logged_in, login_view, logout_view, permission_denied
-from .edit_views import aux_form_view, history_view, new_diagnosis_view, new_treatment_view, patient_edit_form_view, encounter_edit_form_view, patient_export_view, patient_medical, new_vitals_view
+from .edit_views import aux_form_view, history_view, new_diagnosis_view, new_treatment_view, patient_edit_form_view, encounter_edit_form_view, patient_export_view, patient_medical, new_vitals_view, upload_photo_view
 from .form_views import patient_form_view, referral_form_view, patient_encounter_form_view
 from .list_views import chief_complaint_list_view, patient_csv_export_view, patient_list_view, search_patient_list_view, filter_patient_list_view
 from .views import forgot_username, index, home, healthcheck, help_messages_off
@@ -68,6 +68,8 @@ urlpatterns = [
          history_view, name='history_view'),
     path(r'new_treatment_view/<int:patient_id>/<int:encounter_id>',
          new_treatment_view, name='new_treatment_view'),
+    path(r'upload_photo_view/<int:patient_id>/<int:encounter_id>',
+         upload_photo_view, name='upload_photo_view'),
 
     path(r'patient_medical/<int:id>', patient_medical, name='patient_medical'),
 
