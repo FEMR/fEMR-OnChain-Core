d915c30e36ddb740b0db71a8cdcafb73ea4240e8
Sean Batzel <romulus108@protonmail.com>
2021-07-29 13:37:25 -0400

    Error fix.

--- main/views.py
+++ main/views.py
@@ -43,6 +43,8 @@ def home(request):
                 motd_final = motd.text
             else:
                 motd_final = ""
+        else:
+            motd_final = ""
         return render(request, 'data/home.html', {'user': request.user,
                                                   'page_name': 'Home',
                                                   'campaigns': request.user.campaigns.filter(active=True),
