9aec2e320ab9492cb3aab8281c2f45136ff75ced
Sean Batzel <romulus108@protonmail.com>
2022-03-04 21:09:22 -0500

    Updating admin options.

--- main/management/commands/adminoptions.py
+++ main/management/commands/adminoptions.py
@@ -7,28 +7,26 @@ from django.core.management.base import BaseCommand
 from main.models import AdministrationSchedule
 
 OPTIONS = [
-    "alt",
-    "BID",
-    "BIW",
-    "CID",
-    "HS",
-    "q12",
-    "q24",
-    "q4-6",
-    "q4",
-    "q6",
-    "q8",
-    "qAM",
-    "qd",
-    "qHS",
-    "QID",
-    "q5min",
-    "qOd",
-    "qPM",
-    "qWeek",
-    "TID",
-    "TIW",
-    "prn",
+    ["BID", 2],
+    ["BIW", (2/7)],
+    ["HS", 1],
+    ["q12", 2],
+    ["q24", 1],
+    ["q4-6", 6],
+    ["q4", 6],
+    ["q6", 4],
+    ["q8", 1],
+    ["qAM", 1],
+    ["qd", 1],
+    ["qHS", 1],
+    ["QID", 4],
+    ["q5min", 288],
+    ["qOd", (1/2)],
+    ["qPM", 1],
+    ["qWeek", 1],
+    ["TID", 3],
+    ["TIW", (3/7)],
+    ["prn", 5],
 ]
 
 
@@ -46,5 +44,4 @@ class Command(BaseCommand):
         @return:
         """
         for option in OPTIONS:
-            if not AdministrationSchedule.objects.filter(text=option).exists():
-                AdministrationSchedule.objects.create(text=option)
+            AdministrationSchedule.objects.update_or_create(text=option[0], modifier=option[1])
