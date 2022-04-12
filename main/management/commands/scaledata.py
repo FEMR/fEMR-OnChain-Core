98dd0436bbd49b3d9b9594a7f8d845d558ff308b
Sean Batzel <romulus108@protonmail.com>
2022-04-12 13:57:52 -0400

    Progress reporting for long-running tasks.

--- main/management/commands/scaledata.py
+++ main/management/commands/scaledata.py
@@ -3,7 +3,7 @@ Defines a createadmin command extending manage.py.
 """
 
 from django.core.management.base import BaseCommand
-from main.models import Campaign
+from main.models import Campaign, Patient
 from model_bakery import baker
 
 
@@ -21,11 +21,16 @@ class Command(BaseCommand):
         @return:
         """
         campaign = Campaign.objects.get_or_create("Test")[0]
-        for _ in range(1000):
-            patient = baker.make("main.Patient")
-            patient.campaign.add(campaign)
-            for _ in range(10):
-                encounter = baker.make("main.PatientEncounter")
-                encounter.patient = patient
-                encounter.campaign = campaign
-                encounter.save()
+        if Patient.objects.filter(campaign=campaign).count() == 0:
+            print("Generating patient data.")
+            for p in range(1000):
+                patient = baker.make("main.Patient")
+                patient.campaign.add(campaign)
+                for _ in range(10):
+                    encounter = baker.make("main.PatientEncounter")
+                    encounter.patient = patient
+                    encounter.campaign = campaign
+                    encounter.save()
+                print(f"Patient {p} finished.")
+        else:
+            print("Database already populated.")
