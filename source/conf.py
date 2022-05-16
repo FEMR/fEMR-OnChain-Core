a54006b8ae9e199ac4d22a3cbc6e33fb4aa5a165
Sean Batzel <romulus108@protonmail.com>
2022-05-16 19:57:43 -0400

    Version rebadge.

--- source/conf.py
+++ source/conf.py
@@ -27,7 +27,7 @@ copyright = "2021, Team fEMR"
 author = "Team fEMR"
 
 # The full version, including alpha/beta/rc tags
-release = "v1.6.0"
+release = "v1.6.1"
 
 # -- General configuration ---------------------------------------------------
 
